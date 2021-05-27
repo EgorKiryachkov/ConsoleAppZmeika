1974c0e1355715f35db927fdc98ece73c46086f6
EgorKiryachkov <egor00015@gmail.com>
2021-05-27 10:53:40 +0300

    Создал два конструктора

--- Point.cs
+++ Point.cs
@@ -9,6 +9,17 @@ namespace ConsoleAppZmeika
         public int x;
         public int y;
         public char sym;
+
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
