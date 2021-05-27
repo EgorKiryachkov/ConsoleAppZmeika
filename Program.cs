ce578729b2a5fb38713eaaa61a5a146ad9b57874
EgorKiryachkov <egor00015@gmail.com>
2021-05-27 13:02:20 +0300

    Создал класс VerticalLine, сделал рамку, добавил класс Figure, сделал наследование функций класса

--- Program.cs
+++ Program.cs
@@ -10,16 +10,19 @@ namespace ConsoleAppZmeika
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25); //фикс ошибки с размером окна
+            Console.SetBufferSize(80, 25);
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
-
-            Console.ReadLine();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
         }
     }
 }
