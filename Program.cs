bfba9b1de3251dfd87aac7ef6312b88a521d977b
EgorKiryachkov <egor00015@gmail.com>
2021-06-10 12:24:22 +0300

    Игра закончена

--- Program.cs
+++ Program.cs
@@ -51,6 +51,27 @@ namespace ConsoleAppZmeika
                     zmeika.HandleKey(key.Key);
                 }
             }
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("G A M E  O V E R", xOffset + 5, yOffset++);
+            yOffset++;
+            WriteText("Author: Egor Kiryachkov", xOffset + 2, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
